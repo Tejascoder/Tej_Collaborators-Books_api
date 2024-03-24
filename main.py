6f8ea5c98bd76bb83ff8b1d3af629bccf40f7a10
Tejas <40211554+Tejascoder@users.noreply.github.com>
2024-03-24 05:39:41 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# ====    This is going to become a BOOk Store ! ========
+from fastapi import FastAPI
+
+if __name__ == "__main__":
+    app = FastAPI()
---
+++ routes.py
@@ -0,0 +1,5 @@
+from Bookstore.main import app as Book
+
+@Book.get("/Books")
+def get_books():
+    return {"Topic": "Books"}
