847df5af114d7a7537a34279b9b18e627d379683
Tejas <40211554+Tejascoder@users.noreply.github.com>
2024-03-24 22:07:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,3 @@ if __name__ == "__main__":
     app = FastAPI()
     app.include_router(bookdisplay)
 # ====    This is going to become a BOOk Store ! ========
-from fastapi import FastAPI
-
-if __name__ == "__main__":
-    app = FastAPI()
